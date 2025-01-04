f1b32e50cd5d8390c740a1678dd4f4ce9f8e5936
jbleyel <jbleyel@gmx.net>
2025-01-04 21:44:29 +0100

    * remove workaround for 5002

--- plugin/controllers/models/services.py
+++ plugin/controllers/models/services.py
@@ -424,10 +424,7 @@ def getChannels(idbouquet, stype):
 
 		if chan['ref'].split(":")[1] == '320':  # Hide hidden number markers
 			continue
-		if chan['ref'].split(":")[0] == '5002':  # BAD fix !!! this needs to fix in enigma2 !!!
-			chan['name'] = filterName(channel[0].split(":")[-1])
-		else:
-			chan['name'] = filterName(channel[1])
+		chan['name'] = filterName(channel[1])
 		# IPTV
 		ref = chan['ref']
 		isStreamRelay = f"%3a{streamrelayport}/" in ref
