oe-alliance/OpenWebif

commit 14d2f62d5e211c4d72df28c4667148f5b9a9484d

Use distro's own picon resolver if possible

plugin/controllers/models/services.py
@@ -41,6 +41,15 @@ from ..i18n import _, tstrings
 from ..defaults import PICON_PATH, STREAMRELAY, LCNSUPPORT
 from .epg import EPG, convertGenre, getIPTVLink, filterName, convertDesc, GetWithAlternative
 
+try:
+	from Components.Renderer.Picon import piconLocator
+	getPiconName = piconLocator.getPiconName
+except ImportError:
+	try:
+		from Components.Renderer.Picon import getPiconName
+	except ImportError:
+		getPiconName = None
+
 
 def getServiceInfoString(info, what):
 	v = info.getInfo(what)
@@ -1191,6 +1200,9 @@ def getPicon(sname, pp=None, defaultpicon=True):
 	if pp is None:
 		pp = PICON_PATH
 	if pp is not None:
+		if getPiconName is not None:  # use distro own picon resolver
+			return sname and (p := piconLocator.getPiconName(sname)) is not None and p.replace(pp, PIC) or (DEFAULTPIC if defaultpicon else None)
+
 		# remove URL part
 		if ("://" in sname) or ("%3a//" in sname) or ("%3A//" in sname):
 			cname = unquote(sname.split(":")[-1])

commit 38989306bb3a5caf0429216cba4120d5e3b284dc

[services.py] fix possible exception in some distros caused by https://github.com/oe-alliance/OpenWebif/commit/14d2f62d5e211c4d72df28c4667148f5b9a9484d

plugin/controllers/models/services.py
@@ -1201,7 +1201,7 @@ def getPicon(sname, pp=None, defaultpicon=True):
 		pp = PICON_PATH
 	if pp is not None:
 		if getPiconName is not None:  # use distro own picon resolver
-			return sname and (p := piconLocator.getPiconName(sname)) is not None and p.replace(pp, PIC) or (DEFAULTPIC if defaultpicon else None)
+			return sname and (p := getPiconName(sname)) is not None and p.replace(pp, PIC) or (DEFAULTPIC if defaultpicon else None)
 
 		# remove URL part
 		if ("://" in sname) or ("%3a//" in sname) or ("%3A//" in sname):